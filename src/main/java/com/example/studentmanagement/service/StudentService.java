756f8315a8f24e27a94ae8f63dc85c936d745be2
ShabanNastya <62402810+ShabanNastya@users.noreply.github.com>
2023-06-06 23:14:51 +0300

    add a student and show all students

---
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.example.studentmanagement.service;
+
+import com.example.studentmanagement.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+}
---
+++ src/main/java/com/example/studentmanagement/service/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.studentmanagement.service;
+
+import com.example.studentmanagement.entity.Student;
+import com.example.studentmanagement.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+}
