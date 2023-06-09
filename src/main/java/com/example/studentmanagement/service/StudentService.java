490b4915e5c7b35a211b8556c534238152feeeef
ShabanNastya <62402810+ShabanNastya@users.noreply.github.com>
2023-06-09 16:41:53 +0300

    edit and delete function

--- src/main/java/com/example/studentmanagement/service/StudentService.java
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -7,5 +7,11 @@ import java.util.List;
 public interface StudentService {
     List<Student> getAllStudents();
 
+    Student getStudentById(Long id);
+
     Student saveStudent(Student student);
+
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
 }
--- src/main/java/com/example/studentmanagement/service/StudentServiceImpl.java
+++ src/main/java/com/example/studentmanagement/service/StudentServiceImpl.java
@@ -20,7 +20,22 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
     public Student saveStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
     }
 }
